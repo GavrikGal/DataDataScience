a7f4d454e955b23aff361ebaabf0cf177ca8e7e6
GavrikGal <gavrik.gal@gmail.com>
2022-09-18 17:33:02 +0300

    ch13_2

--- ch13/ch13_2.py
+++ ch13/ch13_2.py
@@ -1,3 +1,3 @@
 from io import BytesIO      # Необходимо трактовать байты как файл.
 import requests             # Для скачивания файлов, которые
-import tarfile              # находятся в формате .tar.bz
+import tarfile              # находятся в формате .tar.bz d
